079489ad40ffdc1c1d7d04679c65644870e646ce
Mattsi <mattsi.jansky@codurance.com>
2022-07-16 11:43:30 +0100

    First basic test

--- valid-sudoku/src/lib.rs
+++ valid-sudoku/src/lib.rs
@@ -1,8 +1,9 @@
 struct Solution {}
 
 impl Solution {
+    //Using a ``Vec<Vec<char>>` is bad but this is the signiature leetcode uses, don't get a choice
     pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
-        unimplemented!()
+        true
     }
 }
 
@@ -10,5 +11,20 @@ impl Solution {
 mod tests {
     use super::*;
 
+    #[test]
+    fn given_blank_board_is_valid_sudoku() {
+        let input = vec![
+            vec!['.','.','.','.','.','.','.','.','.',],
+            vec!['.','.','.','.','.','.','.','.','.',],
+            vec!['.','.','.','.','.','.','.','.','.',],
+            vec!['.','.','.','.','.','.','.','.','.',],
+            vec!['.','.','.','.','.','.','.','.','.',],
+            vec!['.','.','.','.','.','.','.','.','.',],
+            vec!['.','.','.','.','.','.','.','.','.',],
+            vec!['.','.','.','.','.','.','.','.','.',],
+            vec!['.','.','.','.','.','.','.','.','.',],
+        ];
 
+        assert_eq!(true, Solution::is_valid_sudoku(input));
+    }
 }

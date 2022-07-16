93bc61095acf26eac41bf19bd4e5830693725c7d
Mattsi <mattsi.jansky@codurance.com>
2022-07-16 11:14:07 +0100

    Add valid-sudoku problem starter

---
+++ valid-sudoku/src/lib.rs
@@ -0,0 +1,14 @@
+struct Solution {}
+
+impl Solution {
+    pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
+        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
+}

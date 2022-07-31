167aabda192628b52b16c55142349cc47696b908
Mattsi <mattsi.jansky@codurance.com>
2022-07-31 11:54:38 +0100

    Sudoku duplicate numbers in row returns false

--- valid-sudoku/src/lib.rs
+++ valid-sudoku/src/lib.rs
@@ -1,9 +1,27 @@
+use std::collections::{HashMap, HashSet};
+
 struct Solution {}
 
 impl Solution {
     //Using a ``Vec<Vec<char>>` is bad but this is the signiature leetcode uses, don't get a choice
     pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
-        true
+        let mut repetitions = HashMap::new();
+        let mut result = true;
+
+        for (y, row) in board.iter().enumerate() {
+            repetitions.insert(y, HashSet::new());
+            for (x, cell) in row.iter().enumerate() {
+                if cell != &'.' && repetitions.get(&y).unwrap().contains(cell) {
+                    println!("Failing on {} in {},{}", cell, x, y);
+                    result = false;
+                }
+                else {
+                    repetitions.get_mut(&y).unwrap().insert(cell);
+                }
+            }
+        }
+
+        result
     }
 }
 
@@ -43,5 +61,22 @@ mod tests {
         ];
 
         assert_eq!(true, Solution::is_valid_sudoku(input));
+    }
+
+    #[test]
+    fn given_duplicate_numbers_in_row_returns_false() {
+        let input = vec![
+            vec!['3', '3', '.', '.', '7', '.', '.', '.', '.'],
+            vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+            vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+            vec!['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+            vec!['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+            vec!['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+            vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+            vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+            vec!['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+        ];
+
+        assert_eq!(false, Solution::is_valid_sudoku(input));
     }
 }

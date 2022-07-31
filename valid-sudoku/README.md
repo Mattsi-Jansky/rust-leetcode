f2c60f6315a408e0458eded29c9a0e6636f0b4dc
Mattsi <mattsi.jansky@codurance.com>
2022-07-31 19:12:01 +0100

    Sudoku: No duplicate numbers in columns

--- valid-sudoku/README.md
+++ valid-sudoku/README.md
@@ -11,6 +11,7 @@ Note:
 
 Example 1:
 
+```
 Input: board =
 [["5","3",".",".","7",".",".",".","."]
 ,["6",".",".","1","9","5",".",".","."]
@@ -22,9 +23,11 @@ Input: board =
 ,[".",".",".","4","1","9",".",".","5"]
 ,[".",".",".",".","8",".",".","7","9"]]
 Output: true
+```
 
 Example 2:
 
+```
 Input: board =
 [["8","3",".",".","7",".",".",".","."]
 ,["6",".",".","1","9","5",".",".","."]
@@ -37,9 +40,12 @@ Input: board =
 ,[".",".",".",".","8",".",".","7","9"]]
 Output: false
 Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+```
 
 Constraints:
 
+```
     board.length == 9
     board[i].length == 9
     board[i][j] is a digit 1-9 or '.'.
+```
--- valid-sudoku/src/lib.rs
+++ valid-sudoku/src/lib.rs
@@ -2,21 +2,24 @@ use std::collections::{HashMap, HashSet};
 
 struct Solution {}
 
+const ROW_LEN: u32 = 9;
+
 impl Solution {
     //Using a ``Vec<Vec<char>>` is bad but this is the signiature leetcode uses, don't get a choice
     pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
-        let mut repetitions = HashMap::new();
+        let mut rows = HashMap::new();
+        let mut columns = HashMap::new();
         let mut result = true;
 
         for (y, row) in board.iter().enumerate() {
-            repetitions.insert(y, HashSet::new());
+            rows.insert(y, HashSet::new());
             for (x, cell) in row.iter().enumerate() {
-                if cell != &'.' && repetitions.get(&y).unwrap().contains(cell) {
-                    println!("Failing on {} in {},{}", cell, x, y);
+                if !columns.contains_key(&x) { columns.insert(x, HashSet::new()); }
+                if cell != &'.' && !rows.get_mut(&y).unwrap().insert(cell) {
                     result = false;
                 }
-                else {
-                    repetitions.get_mut(&y).unwrap().insert(cell);
+                if cell != &'.' && !columns.get_mut(&x).unwrap().insert(cell) {
+                    result = false;
                 }
             }
         }
@@ -75,6 +78,23 @@ mod tests {
             vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
             vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
             vec!['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+        ];
+
+        assert_eq!(false, Solution::is_valid_sudoku(input));
+    }
+
+    #[test]
+    fn given_duplicate_numbers_in_column_returns_false() {
+        let input = vec![
+            vec!['.', '3', '.', '.', '7', '.', '.', '.', '.'],
+            vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+            vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+            vec!['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+            vec!['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+            vec!['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+            vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+            vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+            vec!['.', '3', '.', '.', '.', '.', '.', '7', '9'],
         ];
 
         assert_eq!(false, Solution::is_valid_sudoku(input));
